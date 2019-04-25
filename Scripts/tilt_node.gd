8f86be646e892775933e2390b26ec762bd327185
AmPhIbIaN26 <avichalsolanki@gmail.com>
2019-04-25 22:34:24 +0530

    Improved code for tilt_node

--- Scripts/tilt_node.gd
+++ Scripts/tilt_node.gd
@@ -10,37 +10,37 @@ var angu = 1
 var angd = -1
 
 func _ready():
-	set_process(true)	
+    set_process(true)	
 
 func _input(event):
-	if event is InputEventMouseButton and Input.is_mouse_button_pressed(BUTTON_MIDDLE):
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
+    if event is InputEventMouseButton and Input.is_mouse_button_pressed(BUTTON_MIDDLE):
+        Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    elif event is InputEventMouseButton:
+        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+        
 
-	
-	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-				rotation.x = clamp(rotation.x,-0.2,0.8)
-				rotate_x(event.relative.y * MOUSE_SENSITIVITY)
-				print(rotation.x)
+    
+    if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+        rotation.x = clamp(rotation.x,-0.2,0.8)
+        rotate_x(event.relative.y * MOUSE_SENSITIVITY)
+        print(rotation.x)
 
 func _process(delta):
-	if Input.is_key_pressed(KEY_R):
-		if rotation.x <= (high):	
-			angd += ang_acc * delta			
-			angd = clamp(angd,0,MAX_ANG_SPEED)
-			rotate_x(angd*delta)
-			print(rotation.x)
-			
-	
-	elif Input.is_key_pressed(KEY_F):
-		if rotation.x >= (low):
-			angu += ang_acc * delta
-			angu = clamp(angu,0,MAX_ANG_SPEED)
-			rotate_x(-angu*delta)
-			print(rotation.x)
+    if Input.is_key_pressed(KEY_R):
+        if rotation.x <= (high):	
+            angd += ang_acc * delta			
+            angd = clamp(angd,0,MAX_ANG_SPEED)
+            rotate_x(angd*delta)
+            print(rotation.x)
+            
+    
+    elif Input.is_key_pressed(KEY_F):
+        if rotation.x >= (low):
+            angu += ang_acc * delta
+            angu = clamp(angu,0,MAX_ANG_SPEED)
+            rotate_x(-angu*delta)
+            print(rotation.x)
 
-	else:
-		angu = 0
-		angd = 0
+    else:
+        angu = 0
+        angd = 0
