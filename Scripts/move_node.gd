8afdbba6008b498f0227580c062b13a6ab527875
Sairam Kumar <61322406+sairam4123@users.noreply.github.com>
2021-01-03 11:57:25 +0530

    Add diagonal movement and also remove duplicates

--- Scripts/move_node.gd
+++ Scripts/move_node.gd
@@ -12,7 +12,6 @@ export(float) var MOUSE_SENSITIVITY = 0.001
 var angl = -1
 var angr = 1
 var speed = 0
-var dir = Vector3(0,0,0)
 var pos = Vector2(0,0)
 var crsr = Vector2(0,0)
 
@@ -34,45 +33,39 @@ func _input(event):
 		self.rotate_y(event.relative.x * MOUSE_SENSITIVITY)
 
 func _process(delta):
-	pos = get_viewport().size
+	var pos = get_viewport().get_visible_rect()
 	
-	if (crsr.x < int(pos.x*area_percent)) or Input.is_key_pressed(KEY_A):
+	var new_dir := Vector3.ZERO
+	if mouse_in:
+		if (crsr.x < int(pos.size.x*area_percent)):
+			new_dir.x -= 1
+		if (crsr.x > (pos.size.x-(pos.size.x*area_percent))):
+			new_dir.x += 1
+		if (crsr.y < int(pos.size.y*area_percent)):
+			new_dir.z -= 1
+		if (crsr.y > (pos.size.y-(pos.size.y*area_percent))):
+			new_dir.z += 1
+	
+	if OS.is_window_focused():
+		if Input.is_key_pressed(KEY_A):
+			new_dir.x -= 1
+		if Input.is_key_pressed(KEY_D):
+			new_dir.x += 1
+		if Input.is_key_pressed(KEY_W):
+			new_dir.z -= 1
+		if Input.is_key_pressed(KEY_S):
+			new_dir.z += 1
 
-		dir = Vector3(0,0,0)
-		dir.x += 1
-		speed += acc * delta
-		speed = clamp(speed,0,MAX_SPEED)
-		translate(dir*delta*speed)
+	if new_dir.length() > 1:
+		new_dir = new_dir.normalized()
 	
-	elif (crsr.x > (pos.x-(pos.x*area_percent))) or Input.is_key_pressed(KEY_D):
-
-		dir = Vector3(0,0,0)
-		dir.x -= 1
+	if new_dir and OS.is_window_focused():
+		dir = new_dir
 		speed += acc * delta
-		speed = clamp(speed,0,MAX_SPEED)
-		translate(dir*delta*speed)
-	
-	elif (crsr.y < int(pos.y*area_percent)) or Input.is_key_pressed(KEY_W):
-
-		dir = Vector3(0,0,0)
-		dir.z += 1
-		speed += acc * delta
-		speed = clamp(speed,0,MAX_SPEED)
-		translate(dir*delta*speed)
-	
-	elif (crsr.y > (pos.y-(pos.y*area_percent))) or Input.is_key_pressed(KEY_S):
-
-		dir = Vector3(0,0,0)
-		dir.z -= 1
-		speed += acc * delta
-		speed = clamp(speed,0,MAX_SPEED)
-		translate(dir*delta*speed)
-	
 	else:
-
 		speed -= dec * delta
-		speed = clamp(speed,0,MAX_SPEED)
-		translate(dir*delta*speed)
+	speed = clamp(speed,0,MAX_SPEED)
+	translate_object_local(dir*delta*speed)
 		
 	if Input.is_key_pressed(KEY_E):
 		
